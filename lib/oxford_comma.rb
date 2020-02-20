23abb032e1677acd74c396c30e31b411005d5210
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-02-20 01:09:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,3 +2,15 @@ def oxford_comma(array)
   array.join("")
   array.join(" and ")
 end
+
+def oxford_comma(array)
+  if array.size == 2
+    array.join(" and ")
+  elsif 2 < array.size
+    last_word = array.pop
+   new_list = array.join(", ")
+    new_list << ", and #{last_word}"
+  else
+    array.join
+  end
+end
