f883cbd4c62ba639b861d6b0a446d960824a00f2
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-02-20 01:10:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 2
+    array.join(" and ")
+  elsif 2 < array.size
+    last_word = array.pop
+   new_list = array.join(", ")
+    new_list << ", and #{last_word}"
+  else
+    array.join
+  end
 end
